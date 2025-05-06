7ee50eab01cbac44f078d4bd608d646dc3e69bf7
Dan Ledwith <dledwith@gmail.com>
2025-05-06 17:22:53 -0400

    update User class

--- src/main/java/dledwith/logs/User.java
+++ src/main/java/dledwith/logs/User.java
@@ -1,5 +1,7 @@
 package dledwith.logs;
 
+import java.util.Objects;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,10 +14,62 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
-	private String username;
+	@Column(name="name", nullable=false, unique=true)
+	private String userName;
+	
+	@Column(name="email", nullable=false)
 	private String email;
+	
+	@Column(name="password_hash", nullable=false)
 	private String hashedPassword;
-	
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(userName, other.userName);
+	}
 
 }
