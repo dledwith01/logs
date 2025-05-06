d6e0377f72eb4b5516ff748619648e9b1d4ed0bc
Dan Ledwith <dledwith@gmail.com>
2025-05-06 18:21:13 -0400

    added comments to Utilities class

--- src/main/java/dledwith/logs/Utilities.java
+++ src/main/java/dledwith/logs/Utilities.java
@@ -2,13 +2,27 @@ package dledwith.logs;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * A collection of miscellaneous methods.
+ */
 public class Utilities {
 	
+	/**
+	 * 
+	 * @param plainString String to be hashed
+	 * @return an encrypted version of plainString
+	 */
 	public static String hashString(String plainString) {
 		String salt = BCrypt.gensalt(12);
 		return BCrypt.hashpw(plainString, salt);
 	}
 	
+	/**
+	 * 
+	 * @param plainString
+	 * @param hashedString
+	 * @return If the plainString and the hashedString match, return true. Otherwise return false.
+	 */
 	public static boolean verifyString(String plainString, String hashedString) {
 		return BCrypt.checkpw(plainString, hashedString);
 	}
